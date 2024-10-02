a56358b1d538c3ecabe52f1a3a5992e61db3eae8
mariapricope <30245077@blackpool.ac.uk>
2024-10-02 15:10:26 +0100

    Update AddInv.py

--- AddInv.py
+++ AddInv.py
@@ -10,7 +10,7 @@ class AddInventory:
 
     def __init__(self, inventory):
         """
-        Initialize with an inventory object.
+        Initialise with an inventory object.
         
         :param inventory: The Inventory object to modify.
         """
